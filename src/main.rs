db9aadfccafe446777f1694fb526d6fbd1942899
Eric <ericchang00@gmail.com>
2018-02-27 02:54:25 -0600

    Add debug_print! macro

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,33 @@ use backtrace::Backtrace;
 mod lineinfo;
 use lineinfo::LineInfo;
 
+// TODO: allow these to be configurable by the user
+const SEP_SYMBOL: &str = "|";
+const PAD_SYMBOL: &str = ">";
+
 
 fn short_header(li: &LineInfo) {
-    println!("{num} | {func}()", num = li.lineno(), func = li.funcname());
+    println!("{num} {ss} {func}()",
+             num = li.lineno(),
+             func = li.funcname(),
+             ss = SEP_SYMBOL);
 }
 
 fn full_header(li: &LineInfo) {
-    println!("{num} | {module}::{func}",
+    println!("{num} {ss} {module}::{func}",
              module = li.modname(),
              num = li.lineno(),
-             func = li.funcname());
+             func = li.funcname(),
+             ss = SEP_SYMBOL);
 }
 
 fn fullfull_header(li: &LineInfo) {
-    println!("{num} | {file}::{module}::{func}",
+    println!("{num} {ss} {file}::{module}::{func}",
              file = li.filename(),
              module = li.modname(),
              num = li.lineno(),
-             func = li.funcname());
+             func = li.funcname(),
+             ss = SEP_SYMBOL);
 }
 
 macro_rules! ic_header {
@@ -45,17 +54,32 @@ macro_rules! ic_header {
     );
 }
 
+macro_rules! ic_debug_print {
+    ($x:ident) => (
+        let varname = stringify!($x);
+        println!("{} {} = {:?}", PAD_SYMBOL, varname, $x);
+    );
+}
+
 macro_rules! ic {
     () => ( ic_header!(); );
     (f) => ( ic_header!(f); );
     (ff) => ( ic_header!(ff); );
 
     ($x:ident) => (
-        ic_header!(f);
-        let varname = stringify!($x);
-        println!("{} = {:?}", varname, $x);
+        ic_header!();
+        ic_debug_print!($x);
     );
 
+    ($x:ident, f) => (
+        ic_header!(f);
+        ic_debug_print!($x);
+    );
+
+    ($x:ident, ff) => (
+        ic_header!(ff);
+        ic_debug_print!($x);
+    );
 }
 
 fn main() {
